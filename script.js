e4497f4d71970d68d9f38c130d8502cf48d247f1
Dharma White <dharmawhitebot@gmail.com>
2022-10-02 23:09:03 -0500

    Added length check

--- script.js
+++ script.js
@@ -8,12 +8,16 @@ function generatePassword(){
   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var characters = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "+", "~"];
   var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var passwordLengthPrompt = window.prompt("How many characters do you want your password to have? Please choose between 8-128");
   var characterChoicePrompt = window.prompt("Would you like your password to have special characters? y/n");
   var lowerCasePrompt = window.prompt("Do you want lowercase letters? y/n");
   var upperCasePrompt = window.prompt("Do you want uppercase letters? y/n");
   var numbersPrompt = window.prompt("Do you want your password to have numbers? y/n");
-  var passwordLength = parseInt(passwordLengthPrompt);
+  var passwordLengthString = window.prompt("How many characters do you want your password to have? Please choose between 8-128");
+  var passwordLength = parseInt(passwordLengthString);
+  while (passwordLength < 8 || passwordLength > 128){
+    var newPasswordLengthString = window.prompt("Per my last instruction, your password must be between 8-128 characters dude. Try again");
+    passwordLength = parseInt(newPasswordLengthString);
+  }
   var userWantsLower = lowerCasePrompt === "y"  || lowerCasePrompt === "Y";
   var userWantsUpper = upperCasePrompt === "y" || upperCasePrompt === "Y";
   var userWantsChars = characterChoicePrompt === "y"  || characterChoicePrompt === "Y";
